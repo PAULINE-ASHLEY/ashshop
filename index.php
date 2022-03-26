5bfb4ef844c655a0f61201c784e85df284681089
PAULINE-ASHLEY <ashleyondiek@gmail.com>
2022-03-26 19:27:25 +0300

    Updation of the file index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1> FIRST WORD ON THE PAGE</h1>
+  </body>
+</html>
